51ca5fbc65fe5579e0dbed43f6d881237f00a1a1
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-05-11 12:58:13 +0600

    rate limiter in specific route

--- routes/v1/test.route.js
+++ routes/v1/test.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const toolsController = require("../../controllers/tools.controller");
 const viewCount = require("../../middleware/viewCount");
+const rateLimit = require('../../middleware/limiter');
 
 // router.get("/", (req, res) => {
 //   res.send("tools found ");
@@ -14,6 +15,6 @@ const viewCount = require("../../middleware/viewCount");
 //shorthand:
 router.route("/").get(toolsController.allTools).post(toolsController.sendTools);
 
-router.route("/:id").get(viewCount,toolsController.toolsWithId);
+router.route("/:id").get(rateLimit,viewCount,toolsController.toolsWithId);
 
 module.exports = router;
